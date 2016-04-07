79a6cef5b465ed4d556a2b0ee91e7e47c53a632c
Richard Williams <rw251@yahoo.co.uk>
2016-04-07 20:46:01 +0100

    Fix code to make generic

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 grunt
 
-lftp -e "mirror -R /home/ubuntu/workspace/dist/public_html/ /public_html/cdown && exit" -u $FTP_USER,$FTP_PASS $FTP_SITE
+lftp -e "set ftp:ssl-allow no;mirror -R ./dist/public_html/ /public_html/cdown && exit" -u $FTP_USER,$FTP_PASS $FTP_SITE
 
 #echo "export FTP_USER=xxxx" >> ~/.profile
 #echo "export FTP_PASS=xxxx" >> ~/.profile
